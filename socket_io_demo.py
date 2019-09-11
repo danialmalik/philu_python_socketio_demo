2a2ac35b6fc59daea53fb4e3af1f85b6f91f4c78
danialmalik <danialmalik321@gmail.com>
2019-09-11 13:10:12 +0500

    initial commit

---
+++ socket_io_demo.py
@@ -0,0 +1,44 @@
+import socketio
+
+
+sio = socketio.Client()
+
+
+PHILU_SOCKET_URL = 'wss://community.philanthropyu.org/socket.io'
+SOCKET_EVENT =  'categories.loadMoreCategories'
+# PROXY_PASS = 'community.philanthropyu.org'
+# PROXY_PORT = 443
+
+# JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOjcsImVtYWlsIjoiYWRtaW5AcGhpbGFudGhyb3B5dS5vcmcifQ.ZEPi5-tC_1cC4nIUFYpKJBo3RzG9xPXwpivMAnKl72s'
+
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.on(SOCKET_EVENT)
+def loadCategories(data):
+    print('message received with ', data)
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+
+def callback(data, data2):
+    print(data, data2)
+
+sio.connect(PHILU_SOCKET_URL, headers={
+    # 'token': '{}'.format(JWT_TOKEN)
+}, transports='websocket')
+print('CONNECTED')
+
+
+payload = {
+            'after': 0,
+            'queryParams': ''
+        }
+
+print("SENDING DATA")
+sio.emit(SOCKET_EVENT, payload, callback=callback)
+print("DATA SENT")
